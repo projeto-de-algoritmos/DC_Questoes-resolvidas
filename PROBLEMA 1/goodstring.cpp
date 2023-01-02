edfe258c77b284566731386b81618664e5878b2d
Ericklevy <erickfga23@gmail.com>
2023-01-01 23:31:19 -0300

    adicionando 2 questões codeforces

---
+++ PROBLEMA 1/goodstring.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MAX 131073
+char res[MAX];
+
+int custo(int L , int R , char ajuda);
+int minimo(int L , int R , char ajuda);
+void sol();
+
+
+
+int main()
+{
+	int t ;
+	cin>>t;
+	while(t--)
+	{
+		sol();
+	}
+}
+
+
+int custo(int L , int R , char ajuda)
+{
+	int count = 0;
+	for(int i=L;i<=R;i++)
+	if(res[i] != ajuda) count++;
+	
+	return count;
+}
+
+int minimo(int L , int R , char ajuda)
+{
+	if(L == R)
+	{
+		if(res[L] == ajuda) return 0;
+		else			return 1;
+	}
+	
+	int mid = (L + R) / 2;
+	
+	return min(custo(L , mid , ajuda) + 
+	minimo(mid + 1 , R , ajuda+1) ,
+	custo(mid + 1 , R , ajuda) + 
+	minimo(L , mid , ajuda+1));
+}
+
+void sol(){
+    int n;
+    cin>>n;
+		for (int i = 1; i <= n; i++){
+		    
+		    cin>>res[i];
+		}
+
+		
+		cout<<minimo(1 , n , 'a')<<endl;
+}
+
+
+
+
+
+
+
---
+++ PROBLEMA 2/contest.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+const int MAX = 1000000;
+ 
+int n;
+int k1, k2, k3;
+vector<int> v(MAX);
+int tb[MAX][3];
+ 
+int DC(int idx, int vez = 0) {
+    if(idx >= n) return 0;
+ 
+    if(tb[idx][vez] != -1) return tb[idx][vez]; 
+ 
+    int troca = 1e8, ntroca, tres = 1e8;
+    if(vez == 0) {
+        troca = (v[idx] != 2) + DC(idx+1, 1);
+        tres = (v[idx] != 3) + DC(idx+1, 2);
+        ntroca = (v[idx] != 1) + DC(idx+1);
+    }else if(vez == 1) {
+        troca = (v[idx] != 3) + DC(idx+1, 2);
+        ntroca = (v[idx] != 2) + DC(idx+1, vez);
+    }else {
+        ntroca = (v[idx] != 3) + DC(idx+1, 2);
+    }
+ 
+    return tb[idx][vez] = min({troca, ntroca, tres});
+}
+ 
+int main(){
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    
+    memset(tb, -1, sizeof tb);
+    cin >> k1 >> k2 >> k3;
+    n = k1+k2+k3;
+ 
+    for(int i = 0; i < n; i++) {
+        int a; cin >> a;
+        if(i < k1) {
+            v[a-1] = 1;
+        }else if(i < k1+k2) {
+            v[a-1] = 2;
+        }else {
+            v[a-1] = 3;
+        }   
+    }
+    
+    cout << DC(0);
+ 
+    return 0;
+}
